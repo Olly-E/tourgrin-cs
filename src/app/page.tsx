40d3db49d18d1b75d84fd34c927b88f996197eef
Emmanuel Akinsiku <akinsnuel@gmail.com>
2024-07-15 12:25:11 +0100

    added  coming soon page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -43,7 +43,7 @@ export default function Home() {
           Get Notified
         </Button>
       </div>
-      <div className="flex flex-col gap-10 lg:flex-row items-center justify-between lg:my-14 h-[calc(100dvh-420px)]">
+      <div className="flex flex-col gap-10 lg:flex-row items-center justify-between lg:mb-14 h-[calc(100dvh-420px)]">
         <div className="text-center lg:text-left">
           <h1 className="text-[#E3EECE] text-[80px] sm:text-[100px] leading-[80px] sm:leading-[140px] xl:leading-normal xl:text-[142.05px] font-bold">
             Coming Soon
@@ -54,7 +54,7 @@ export default function Home() {
           <p className="text-[#CDEA89] text-sm sm:text-[18px] lg:text-[25px] font-bold mt-2 sm:mt-4 lg:mt-8">
             Get notified when we launch
           </p>
-          <div className="border border-[#FCFDFA] gap-4 mt-6 rounded-[8px] pl-4 flex items-center mx-auto lg:mx-0 sm:w-[470px] overflow-hidden h-[47px] sm:h-[67px] lg:h-[70px]">
+          <div className="border border-[#FCFDFA] gap-4 mt-10 rounded-[8px] pl-4 flex items-center mx-auto lg:mx-0 sm:w-[470px] overflow-hidden h-[47px] sm:h-[67px] lg:h-[70px]">
             <input
               type="email"
               className="h-full flex-1 w-full border-none bg-transparent outline-none text-white"
@@ -62,7 +62,7 @@ export default function Home() {
             />
             <Button
               type="button"
-              className="bg-[#CDEA89] text-[#052E16] text-sm sm:text-base h-full"
+              className="bg-[#CDEA89] rounded-l-none text-[#052E16] text-sm sm:text-base h-full"
             >
               Subscribe
             </Button>
@@ -78,7 +78,7 @@ export default function Home() {
           />
         </div>
       </div>
-      <div className="flex items-center justify-center text-[#F6FAF0] gap-6 mt-10">
+      <div className="flex items-center justify-center text-[#F6FAF0] gap-6 mt-14">
         {SOCIAL_LINKS.map((item, index) => (
           <Link href={item.link} key={index} className="">
             <Image src={item.icon} alt={item.title} width={24} height={24} />
